542cf538b4395991905b2f377ab68908bc7e6902
SARomanchuk <SARomanchuk@sberbank.ru>
2022-02-23 21:36:46 +0300

    backend

--- celery_app/app.py
+++ celery_app/app.py
@@ -4,6 +4,7 @@ from .config import CELERY_BROKER, CELERY_BACKEND
 app = Celery(
     "teach_me",
     broker=CELERY_BROKER,
+    backend=CELERY_BACKEND,
     include=["celery_app.tasks", ],
 )
 
--- example.py
+++ example.py
@@ -2,9 +2,9 @@
 # from celery_app import worker_app
 from celery import Celery
 app = Celery('teach_me', broker='amqp://guest:guest@192.168.1.9:5672')
-url = "https://magnit.ru"
+url = "https://lenta.ru"
 
 # a = download_page.delay(url)
-b = app.send_task(name="download_page", kwargs={"url": url}, priority=1, ignore_result=True)
+b = app.send_task(name="download_page", kwargs={"url": url}, priority=1)
 # download_page.s(url).apply_async()
 print(1)
