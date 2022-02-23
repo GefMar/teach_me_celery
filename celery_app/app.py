b03e694fb32286136c19f274a8d778ed8a9cdff5
SARomanchuk <SARomanchuk@sberbank.ru>
2022-02-23 21:52:25 +0300

    backend

--- celery_app/app.py
+++ celery_app/app.py
@@ -1,3 +1,5 @@
+import datetime
+
 from celery import Celery
 from .config import CELERY_BROKER, CELERY_BACKEND
 
@@ -8,3 +10,5 @@ app = Celery(
     include=["celery_app.tasks", ],
 )
 app.conf.task_queue_max_priority = 4
+app.conf.task_default_priority = 1
+app.conf.result_expires = datetime.timedelta(hours=2)
