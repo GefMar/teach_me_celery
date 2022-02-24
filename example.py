4704d610842b8f3e0c2c9f3c2c4fb997149ded27
SARomanchuk <SARomanchuk@sberbank.ru>
2022-02-24 16:36:10 +0300

    backend

--- example.py
+++ example.py
@@ -1,9 +1,15 @@
+import datetime
+
 from celery_app.tasks import download_page
 from celery_app import worker_app
 
 # from celery_app import worker_app
 from celery import Celery
 app = Celery('teach_me', broker='amqp://guest:guest@192.168.1.9:5672', backend='redis://192.168.1.9:6379')
+app.conf.task_queue_max_priority = 10
+app.conf.task_default_priority = 1
+app.conf.result_expires = datetime.timedelta(hours=2)
+
 url = "https://lenta.ru"
 
 # a = download_page.delay(url)
