56ec664132fb9fd312763d5bbbdfb899b391dd96
SARomanchuk <SARomanchuk@sberbank.ru>
2022-02-24 16:41:07 +0300

    backend

--- celery_app/app.py
+++ celery_app/app.py
@@ -8,11 +8,11 @@ routes = {
 app = Celery(
     "teach_me",
     broker=CELERY_BROKER,
-    backend=CELERY_BACKEND,
+    # backend=CELERY_BACKEND,
     include=["celery_app.tasks", ],
 )
 
 # app.conf.task_routes = routes
-app.conf.task_queue_max_priority = 10
-app.conf.task_default_priority = 1
-app.conf.result_expires = datetime.timedelta(hours=2)
+# app.conf.task_queue_max_priority = 10
+# app.conf.task_default_priority = 1
+# app.conf.result_expires = datetime.timedelta(hours=2)
